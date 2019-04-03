4d33a5fe83f9ba42904c728f9ab8d7fbacda4a65
Sunny Ouyang <ShenOuyang.work@gmail.com>
2019-04-03 10:53:15 -0700

    configured navigation bar

--- Up/initialViewController.swift
+++ Up/initialViewController.swift
@@ -9,13 +9,36 @@ import UIKit
 
 class initialViewController: UIViewController {
 
+    //MARK: OUTLETS
+    var upTableView = UITableView()
+    
+    
+    //MARK: VARIABLES
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.prefersLargeTitles = true
+        setUp()
+        
         // Do any additional setup after loading the view.
     }
     
-
+    private func setUp() {
+        configNavBar()
+        self.title = "Up"
+        self.view.backgroundColor = #colorLiteral(red: 0.07843137255, green: 0.07843137255, blue: 0.07843137255, alpha: 1)
+    }
    
+    private func configNavBar() {
+        extendedLayoutIncludesOpaqueBars = true
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.07843137255, green: 0.07843137255, blue: 0.07843137255, alpha: 1)
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 
 }
