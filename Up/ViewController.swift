6ccd57ffb00dd1b51c58bcaf9c88f4f9db7009d8
Sunny Ouyang <sunnyouyang@sunnys-mbp.attlocal.net>
2019-04-03 09:15:52 -0700

    first commit

---
+++ Up/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Up
+//
+//  Created by Sunny Ouyang on 4/3/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
