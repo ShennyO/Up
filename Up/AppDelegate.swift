ad26abc35806d679d31cda097e4ae855fb50ad47
Sunny Ouyang <sunnyouyang@sunnys-mbp.attlocal.net>
2019-04-03 09:18:50 -0700

    deleted storyboard, setting up for programmatic

--- Up/AppDelegate.swift
+++ Up/AppDelegate.swift
@@ -11,10 +11,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return self.orientationLock
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let initialVewController = initialViewController()
+        let navigationController = UINavigationController(rootViewController: initialVewController)
+        
+        window!.rootViewController = navigationController
+        window!.makeKeyAndVisible()
         return true
     }
 
---
+++ Up/initialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  initialViewController.swift
+//  Up
+//
+//  Created by Sunny Ouyang on 4/3/19.
+//
+
+import UIKit
+
+class initialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
