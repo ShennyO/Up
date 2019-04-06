e051683c5aee1f67132e428f1b010120b8a26d47
Sunny Ouyang <ShenOuyang.work@gmail.com>
2019-04-06 01:29:37 -0700

    added Outlets to newProjectViewController

--- Up/Controllers/NewProjectViewController.swift
+++ Up/Controllers/NewProjectViewController.swift
@@ -9,26 +9,40 @@ import UIKit
 
 class NewProjectViewController: UIViewController {
     
+    //VARIABLES
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
     //MARK: OUTLETS
     var titleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 30)
         label.numberOfLines = 0
-        label.textColor = #colorLiteral(red: 0.09240043908, green: 0.0924237594, blue: 0.09239736944, alpha: 1)
+        label.textColor = UIColor.white
         return label
     }()
     
-    var titleTextField = UITextField()
+    var titleTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 20)
+        textView.layer.cornerRadius = 3
+        return textView
+    }()
     
-    var descriptionTextView = UITextView()
-    
-    
+    var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 20)
+        textView.layer.cornerRadius = 3
+        return textView
+    }()
     
     var descriptionLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 30)
         label.numberOfLines = 0
-        label.textColor = #colorLiteral(red: 0.09240043908, green: 0.0924237594, blue: 0.09239736944, alpha: 1)
+        label.textColor = UIColor.white
         return label
     }()
     
@@ -41,9 +55,55 @@ class NewProjectViewController: UIViewController {
     }()
     
     
+    //MARK: PRIVATE FUNCTIONS
+    private func configNavBar() {
+        extendedLayoutIncludesOpaqueBars = true
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.07843137255, green: 0.07843137255, blue: 0.07843137255, alpha: 1)
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func addOutlets() {
+        [titleLabel,titleTextView,descriptionLabel,descriptionTextView].forEach { (view) in
+            self.view.addSubview(view)
+        }
+        
+    }
+    
+    private func setConstraints() {
+        titleLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(150)
+        }
+        titleTextView.snp.makeConstraints { (make) in
+            make.left.equalTo(titleLabel.snp.right).offset(15)
+            make.top.equalToSuperview().offset(150)
+            make.height.equalTo(40)
+            make.right.equalToSuperview().offset(-15)
+        }
+        
+        descriptionLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
+            make.left.equalToSuperview().offset(20)
+        }
+        descriptionTextView.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
+            make.left.equalTo(descriptionLabel.snp.right).offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.height.equalTo(100)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configNavBar()
+        addOutlets()
+        setConstraints()
+        titleLabel.text = "Title:"
+        descriptionLabel.text = "Desc:"
         // Do any additional setup after loading the view.
     }
     
