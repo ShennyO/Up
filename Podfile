da154ff6983ecb57d9a0cc6d76b77362a69ece2c
Sunny Ouyang <ShenOuyang.work@gmail.com>
2019-04-17 17:35:48 -0700

    added JTAppleCalendar pod

--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'Up' do
 
   # Pods for Up
     pod 'SnapKit', '~> 4.0.0'
+    pod 'JTAppleCalendar', '~> 7.1'
 
   target 'UpTests' do
     inherit! :search_paths
---
+++ Up/Controllers/CalendarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarViewController.swift
+//  Up
+//
+//  Created by Sunny Ouyang on 4/17/19.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
