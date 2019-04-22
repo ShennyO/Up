f192a356ad0c3060223c5c8fb2b33018566278af
Sunny <ShenOuyang.work@gmail.com>
2019-04-22 15:36:02 -0700

    ADDED - CoreDataStack and CoreData Initializable

---
+++ Up/CoreData/CoreDataFetchHelper.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataFetchHelper.swift
+//  Up
+//
+//  Created by Sunny Ouyang on 4/22/19.
+//
+
+import Foundation
+import CoreData
+
+enum goalType {
+    case timed
+    case untimed
+}
+
+
+
