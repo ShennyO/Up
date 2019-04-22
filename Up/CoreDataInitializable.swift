aeab4f9ff8c4fe3dfbeaac8804f6a345b6f864da
Sunny <ShenOuyang.work@gmail.com>
2019-04-22 14:33:33 -0700

    Reverted back to before adding Core Data

---
+++ Up/CoreDataInitializable.swift
@@ -0,0 +1,34 @@
+//
+//  CoreDataInitializable.swift
+//  Up
+//
+//  Created by Sunny Ouyang on 4/22/19.
+//
+
+import Foundation
+import CoreData
+
+
+protocol CoreDataInitializable {
+    init(context: NSManagedObjectContext)
+}
+
+extension CoreDataInitializable where Self: NSManagedObject {
+    init(context: NSManagedObjectContext) {
+        
+        let entityDescription = NSEntityDescription.entity(
+            forEntityName: NSManagedObject.entityName(), in:
+            context
+            )!
+        
+        self.init(entity: entityDescription, insertInto: context)
+    }
+}
+
+extension NSManagedObject {
+    class func entityName() -> String {
+        let fullClassName = String(describing: self)
+        let nameComponents = fullClassName.split(separator: ".")
+        return String(describing: nameComponents.last)
+    }
+}
