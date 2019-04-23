bad226874e948d556269b49734bc4f5618097fe6
Sunny <ShenOuyang.work@gmail.com>
2019-04-23 16:36:04 -0700

    ADDED - CalendarVC to TabBarController

--- Up/UpTabBarController.swift
+++ Up/UpTabBarController.swift
@@ -18,20 +18,31 @@ class UpTabBarController: UITabBarController, UITabBarControllerDelegate {
         // Do any additional setup after loading the view.
     }
     
+    private func configNavBar(navController: UINavigationController) {
+        extendedLayoutIncludesOpaqueBars = true
+        navController.navigationBar.barTintColor = #colorLiteral(red: 0.07843137255, green: 0.07843137255, blue: 0.07843137255, alpha: 1)
+        navController.navigationBar.isTranslucent = false
+        navController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navController.navigationBar.shadowImage = UIImage()
+        navController.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        //        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         //create tab one
         let goalsVC = UpViewController()
         let goalsNavVC = UINavigationController(rootViewController: goalsVC)
+        configNavBar(navController: goalsNavVC)
         let tabOneBarItem = UITabBarItem(title: "Goals", image: UIImage(named: "defaultImage.png"), selectedImage: UIImage(named: "selectedImage.png"))
         goalsNavVC.tabBarItem = tabOneBarItem
         
         //create tab two
-//        let CalendarVC = Calend
-//        let hatchNavVC = UINavigationController(rootViewController: HatchVC)
-//        let tabTwoBarItem = UITabBarItem(title: "Hatch", image: UIImage(named: "defaultImage.png"), selectedImage: UIImage(named: "selectedImage.png"))
-//        hatchNavVC.tabBarItem = tabTwoBarItem
-        self.viewControllers = [goalsVC]
+        let calendarVC = CalendarViewController()
+        let calendarNavVC = UINavigationController(rootViewController: calendarVC)
+        let tabTwoBarItem = UITabBarItem(title: "Calendar", image: UIImage(named: "defaultImage.png"), selectedImage: UIImage(named: "selectedImage.png"))
+        calendarNavVC.tabBarItem = tabTwoBarItem
+        self.viewControllers = [goalsVC, calendarVC]
     }
     
     
