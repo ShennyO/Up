7a184311a6795434965fb823d651572cfe835272
Sunny <ShenOuyang.work@gmail.com>
2019-04-24 09:09:46 -0700

    ADDED - tab bar item animation when tapped

--- Up/UpTabBarController.swift
+++ Up/UpTabBarController.swift
@@ -10,7 +10,12 @@ import UIKit
 import SnapKit
 
 
+
 class UpTabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    var firstTabbarItemImageView: UIImageView!
+    var secondTabbarItemImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTabBar()
@@ -41,6 +46,8 @@ class UpTabBarController: UITabBarController, UITabBarControllerDelegate {
         goalsNavVC.tabBarItem.image = #imageLiteral(resourceName: "home")
         goalsNavVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "selectedHome")
         goalsNavVC.tabBarItem.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+        goalsNavVC.tabBarItem.tag = 1
+        
         
         
         
@@ -50,11 +57,45 @@ class UpTabBarController: UITabBarController, UITabBarControllerDelegate {
         calendarNavVC.tabBarItem.image = #imageLiteral(resourceName: "calendar")
         calendarNavVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "selectedCalendar")
         calendarNavVC.tabBarItem.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
-        
+        calendarNavVC.tabBarItem.tag = 2
         
         self.viewControllers = [goalsNavVC, calendarNavVC]
+        
+        let firstItemView = tabBar.subviews.first!
+        firstTabbarItemImageView = firstItemView.subviews.first as? UIImageView
+        firstTabbarItemImageView.contentMode = .center
+        
+        let secondItemView = self.tabBar.subviews[1]
+        self.secondTabbarItemImageView = secondItemView.subviews.first as? UIImageView
+        self.secondTabbarItemImageView.contentMode = .center
+
+        
     }
     
+    private func animate(_ imageView: UIImageView) {
+        UIView.animate(withDuration: 0.1, animations: {
+            imageView.transform = CGAffineTransform(scaleX: 1.15, y: 1.15)
+        }) { _ in
+            UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 2.0, options: .curveEaseInOut, animations: {
+                imageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: nil)
+        }
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+        switch item.tag {
+        case 1:
+            animate(firstTabbarItemImageView)
+        case 2:
+            animate(secondTabbarItemImageView)
+        default:
+            return
+        }
+        
+        
+    }
+//
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
