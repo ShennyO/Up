82e79355ba1b3eb1d16c215033087dac7d8b0675
Tony Cioara <tonyangelo9707@gmail.com>
2019-07-28 14:58:06 -0700

    Finished functions for getting chart Data

---
+++ Up/ControllersAndViews/StatsVC/ChartsDataSource.swift
@@ -0,0 +1,111 @@
+//
+//  ChartsDataSource.swift
+//  Up
+//
+//  Created by Tony Cioara on 7/27/19.
+//
+
+import Foundation
+
+class ChartsDataSource {
+    static let shared = ChartsDataSource()
+    
+    var goals = [Goal]()
+    let coreDataStack = CoreDataStack.instance
+    
+    private var sixMonthStartIndex: Int?
+    private var thirtyDayStartIndex: Int?
+    private var sevenDayStartIndex: Int?
+    
+    private lazy var gregorian : NSCalendar = {
+        let cal = NSCalendar(identifier: NSCalendar.Identifier.gregorian)!
+        cal.timeZone = TimeZone.current
+        return cal
+    }()
+    
+    init() {
+        fetchData()
+    }
+    
+    func getSixMonthData() -> [Int] {
+        let currentMonth = gregorian.component(.month, from: Date())
+        var result = [Int](repeating: 0, count: 6)
+        guard let sixMonthStartIndex = sixMonthStartIndex else {
+            return result
+        }
+        for index in sixMonthStartIndex..<goals.count {
+            let goal = goals[index]
+            if let completionDate = goal.completionDate {
+                let month = gregorian.component(.month, from: completionDate)
+                let index = 5 - currentMonth + month
+                result[index] += 1
+            }
+        }
+        return result
+    }
+    
+    func getThirtyDayData() -> [Int] {
+        let currentDay = gregorian.startOfDay(for: Date())
+        var result = [Int](repeating: 0, count: 30)
+        guard let thirtyDayStartIndex = thirtyDayStartIndex else {
+            return result
+        }
+        for index in thirtyDayStartIndex..<goals.count {
+            let goal = goals[index]
+            if let completionDate = goal.completionDate {
+                let day = gregorian.startOfDay(for: completionDate)
+                let components = gregorian.components(.day, from: day, to: currentDay, options: NSCalendar.Options())
+                let index = 29 - (components.day ?? 0)
+                result[index] += 1
+            }
+        }
+        return result
+    }
+    
+    func getSevenDayData() -> [Int] {
+        let currentDay = gregorian.startOfDay(for: Date())
+        var result = [Int](repeating: 0, count: 7)
+        guard let sevenDayStartIndex = sevenDayStartIndex else {
+            return result
+        }
+        for index in sevenDayStartIndex..<goals.count {
+            let goal = goals[index]
+            if let completionDate = goal.completionDate {
+                let day = gregorian.startOfDay(for: completionDate)
+                let components = gregorian.components(.day, from: day, to: currentDay, options: NSCalendar.Options())
+                let index = 6 - (components.day ?? 0)
+                result[index] += 1
+            }
+        }
+        return result
+    }
+    
+    private func fetchData() {
+        let now = Date()
+        goals = coreDataStack.fetchGoal(type: .all, completed: .completed, sorting: .completionDateAscending) as! [Goal]
+        for index in 0..<goals.count {
+            let goal = goals[index]
+            if let completionDate = goal.completionDate {
+                if sixMonthStartIndex == nil && moreThan(unit: .month, value: 1, from: completionDate, to: now) {
+                    sixMonthStartIndex = index
+                }
+                if thirtyDayStartIndex == nil && moreThan(unit: .day, value: 30, from: completionDate, to: now) {
+                    thirtyDayStartIndex = index
+                }
+                if sevenDayStartIndex == nil && moreThan(unit: .day, value: 7, from: completionDate, to: now) {
+                    sevenDayStartIndex = index
+                }
+            }
+            
+        }
+    }
+    
+    private func moreThan(unit: NSCalendar.Unit, value: Int, from: Date, to: Date) -> Bool {
+        let startDate = gregorian.date(byAdding: unit, value: -value, to: to, options: NSCalendar.Options())!
+        let comparison = gregorian.compare(from, to: startDate, toUnitGranularity: .day)
+        if comparison == .orderedAscending {
+            return false
+        }
+        return true
+    }
+}
--- Up/ControllersAndViews/StatsVC/StatsViewController.swift
+++ Up/ControllersAndViews/StatsVC/StatsViewController.swift
@@ -105,7 +105,9 @@ class StatsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ChartsDataSource.shared.getYearData()
+        ChartsDataSource.shared.getSixMonthData()
+        ChartsDataSource.shared.getThirtyDayData()
+        ChartsDataSource.shared.getSevenDayData()
         chartSetup(chart: &chart, chartData:  [0, 5.5, 2, 3, 6, 4.1, 3, -10], xLabels: allHours)
         chartSegmentedControl.selectedSegmentIndex = 0
         configNavBar()
